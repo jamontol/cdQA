50a5f55e5810d93e3243956f2ea1b3d3ab960042
jrml <Javier.Monreal@gft.com>
2019-12-19 13:24:25 +0100

    cambios para incluir LSA_retriver

--- cdqa/retriever/__init__.py
+++ cdqa/retriever/__init__.py
@@ -1,3 +1,3 @@
-from .retriever_sklearn import TfidfRetriever, BM25Retriever
+from .retriever_sklearn import TfidfRetriever, BM25Retriever, LSARetriever
 
 __all__ = ["TfidfRetriever", "BM25Retriever", "LSARetriever"]
