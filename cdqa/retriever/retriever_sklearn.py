aef32722bfb37c030721897a9bf26ced591e27c0
jrml <Javier.Monreal@gft.com>
2020-01-07 12:13:17 +0100

    incluida nltk en retriver_sklearn.py

--- cdqa/retriever/retriever_sklearn.py
+++ cdqa/retriever/retriever_sklearn.py
@@ -7,7 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.base import BaseEstimator
 from .vectorizers import BM25Vectorizer
-
+import nltk
 
 
 
