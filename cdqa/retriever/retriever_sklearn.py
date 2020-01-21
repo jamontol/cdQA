20b015a600db76823961646290b3ab13ac3ac3c8
jrml <Javier.Monreal@gft.com>
2020-01-21 17:13:33 +0100

    cambio en tfidf_LSA

--- cdqa/retriever/retriever_sklearn.py
+++ cdqa/retriever/retriever_sklearn.py
@@ -162,7 +162,7 @@ class TfidfRetriever(BaseRetriever):
 
     def __init__(
         self,
-        lowercase=True,
+        lowercase=False,
         preprocessor=None,
         tokenizer=None,
         stop_words=None,
@@ -349,7 +349,7 @@ class LSARetriever(BaseRetriever):
     # We should also try lancaster lemmatizer since it is a more aggresive
     # stemming and word-level tfidf could benefit more from it
   
-    def __init__(self, top_n=20, analyzer='word', ngram_range=(1,3), n_components=120, do_stem=True, verbose=0):
+    def __init__(self, top_n=20, analyzer='word', ngram_range=(1,3), n_components=120, do_stem=True, verbose=False):
 
         self.stemmer = SnowballStemmer('spanish')
         self.stopwords = stopwords.words('spanish')
